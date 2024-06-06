f613d4a085becf9c9ac79add1159a93b4b02f226
Arruda Junior <juniorarruda15@gmail.com>
2024-06-06 16:13:17 -0300

    Adicionando e removendo classes active do button

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const longoButao = document.querySelector('.app__card-button--longo');
 const displayTempo = document.querySelector('#timer');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
 const botaoIniciar = document.querySelector('.app__card-primary-button');
 const duracaoFoco = 1500; 
 const duracaoDescansoCurto = 300; 
@@ -12,17 +13,23 @@ const duracaoDescansoLongo = 900;
 
 focoButao.addEventListener('click', () => {
    alterarContexto('foco');
+   focoButao.classList.add('active');
 })
 
 curtoButao.addEventListener('click', () => {
    alterarContexto('descanso-curto');
+   curtoButao.classList.add('active');
 })
 
 longoButao.addEventListener('click', () => {
    alterarContexto('descanso-longo');
+   longoButao.classList.add('active');
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto) {
