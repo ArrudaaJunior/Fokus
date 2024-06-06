744110156b49324f3c95624f75987dabab2ac027
Arruda Junior <juniorarruda15@gmail.com>
2024-06-06 15:35:05 -0300

    Alterando imagens ao clicar

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const html = document.querySelector('html');
 const focoButao = document.querySelector('.app__card-button--foco');
 const curtoButao = document.querySelector('.app__card-button--curto');
 const longoButao = document.querySelector('.app__card-button--longo');
+const displayTempo = document.querySelector('#timer');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const botaoIniciar = document.querySelector('.app__card-primary-button');
+const duracaoFoco = 1500; 
+const duracaoDescansoCurto = 300; 
+const duracaoDescansoLongo = 900; 
 
 focoButao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png');
 })
 
 curtoButao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
 })
 
 longoButao.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png');
 })
