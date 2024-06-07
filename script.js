5b7b6373ad08dd37b140637aae81f06e97ee7225
Arruda Junior <juniorarruda15@gmail.com>
2024-06-07 17:14:12 -0300

    Alternando texto com textContent e trocando o icone de play e pause

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const html = document.querySelector('html');
 const focoButao = document.querySelector('.app__card-button--foco');
 const curtoButao = document.querySelector('.app__card-button--curto');
 const longoButao = document.querySelector('.app__card-button--longo');
-const displayTempo = document.querySelector('#timer');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBotao = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
+const iniciarOuPausarBotao = document.querySelector('#start-pause span');
+const iconePlayOuPause = document.querySelector('.app__card-primary-butto-icon');
+
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
 const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
 const audioTempoFinalizado = new Audio('/sons/beep.mp3');
-const startPauseBotao = document.querySelector('#start-pause');
+
 
 let tempoDecorridoEmSegundos = 5;
 let intervaloId = null;
@@ -70,7 +73,7 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
-        audioTempoFinalizado.play()
+        // audioTempoFinalizado.play()
         alert('Tempo finalizado');
         zerar();
         return;
@@ -90,9 +93,13 @@ function iniciarOuPausar() {
     }
     audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOuPausarBotao.textContent = "Pausar";
+    iconePlayOuPause.setAttribute('src', `/imagens/pause.png`);
 }
 
 function zerar() {
     clearInterval(intervaloId);
+    iniciarOuPausarBotao.textContent = "Começar"
+    iconePlayOuPause.setAttribute('src', `/imagens/play_arrow.png`);
     intervaloId = null;
 }
