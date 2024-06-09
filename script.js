de31087e545c6a51930b271bbb8626325907af49
Arruda Junior <juniorarruda15@gmail.com>
2024-06-09 17:49:13 -0300

    Adicionado o temporizador + comentários no código

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Seleciona elementos HTML específicos usando seletores CSS.
 const html = document.querySelector('html');
 const focoButao = document.querySelector('.app__card-button--foco');
 const curtoButao = document.querySelector('.app__card-button--curto');
@@ -11,103 +12,118 @@ const iniciarOuPausarBotao = document.querySelector('#start-pause span');
 const iconePlayOuPause = document.querySelector('.app__card-primary-butto-icon');
 const tempoNaTela = document.querySelector('#timer');
 
-
+// Carrega arquivos de áudio para serem usados como música de fundo e efeitos sonoros.
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
 const audioPlay = new Audio('/sons/play.wav');
 const audioPausa = new Audio('/sons/pause.mp3');
 const audioTempoFinalizado = new Audio('/sons/beep.mp3');
 
+// Define variáveis de controle para o temporizador e o intervalo de contagem regressiva.
+let tempoDecorridoEmSegundos = 1500;  // Tempo inicial de 25 minutos (1500 segundos).
+let intervaloId = null;  // Armazena o ID do intervalo de contagem regressiva.
 
-let tempoDecorridoEmSegundos = 1500;
-let intervaloId = null;
+musica.loop = true;  // Faz a música tocar em loop.
 
-musica.loop = true;
-
+// Adiciona um ouvinte de evento ao botão de alternar música.
 musicaFocoInput.addEventListener('change', () => {
-    if(musica.paused) {
-        musica.play();
-    }else{
-        musica.pause();
+    if (musica.paused) {
+        musica.play();  // Toca a música se estiver pausada.
+    } else {
+        musica.pause();  // Pausa a música se estiver tocando.
     }
-})
+});
 
+// Adiciona ouvintes de evento para os botões de foco, descanso curto e descanso longo.
 focoButao.addEventListener('click', () => {
-   alterarContexto('foco');
-   focoButao.classList.add('active');
-})
+    tempoDecorridoEmSegundos = 1500;  // Define o tempo para 25 minutos.
+    alterarContexto('foco');  // Altera o contexto para foco.
+    focoButao.classList.add('active');  // Adiciona a classe 'active' ao botão de foco.
+});
 
 curtoButao.addEventListener('click', () => {
-   alterarContexto('descanso-curto');
-   curtoButao.classList.add('active');
-})
+    tempoDecorridoEmSegundos = 300;  // Define o tempo para 5 minutos.
+    alterarContexto('descanso-curto');  // Altera o contexto para descanso curto.
+    curtoButao.classList.add('active');  // Adiciona a classe 'active' ao botão de descanso curto.
+});
 
 longoButao.addEventListener('click', () => {
-   alterarContexto('descanso-longo');
-   longoButao.classList.add('active');
-})
+    tempoDecorridoEmSegundos = 900;  // Define o tempo para 15 minutos.
+    alterarContexto('descanso-longo');  // Altera o contexto para descanso longo.
+    longoButao.classList.add('active');  // Adiciona a classe 'active' ao botão de descanso longo.
+});
 
+// Função para alterar o contexto da aplicação.
 function alterarContexto(contexto) {
-    botoes.forEach(function (contexto){
-        contexto.classList.remove('active')
-    })
-    html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    mostrarTempo();  // Atualiza o tempo mostrado na tela.
+    botoes.forEach(function (botao) {
+        botao.classList.remove('active');  // Remove a classe 'active' de todos os botões.
+    });
+    html.setAttribute('data-contexto', contexto);  // Define um atributo de dados no elemento HTML.
+    banner.setAttribute('src', `/imagens/${contexto}.png`);  // Altera a imagem do banner.
     switch (contexto) {
         case 'foco':
             titulo.innerHTML = `
             Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>
-            `
+            `;
             break;
         case 'descanso-curto':
             titulo.innerHTML = `
             Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
-            `
+            `;
             break;
         case 'descanso-longo':
             titulo.innerHTML = `
             Hora de voltar à superfície. <strong class="app__title-strong">Faça uma pausa longa.</strong>
-            `
+            `;
+            break;
         default:
             break;
     }
 }
 
+// Função de contagem regressiva do temporizador.
 const contagemRegressiva = () => {
-    if(tempoDecorridoEmSegundos <= 0){
-        // audioTempoFinalizado.play()
-        alert('Tempo finalizado');
-        zerar();
+    if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play();  // Toca o som de tempo finalizado.
+        alert('Tempo finalizado');  // Exibe um alerta.
+        zerar();  // Zera o temporizador.
         return;
     }
-    tempoDecorridoEmSegundos -= 1
-    mostrarTempo();
-}
+    tempoDecorridoEmSegundos -= 1;  // Decrementa o tempo.
+    mostrarTempo();  // Atualiza o tempo mostrado na tela.
+};
 
+// Adiciona ouvinte de evento ao botão de iniciar/pausar.
 startPauseBotao.addEventListener('click', iniciarOuPausar);
 
+// Função para iniciar ou pausar o temporizador.
 function iniciarOuPausar() {
-    if(intervaloId){
-        audioPausa.play();
-        zerar();
+    if (intervaloId) {
+        audioPausa.play();  // Toca o som de pausa.
+        zerar();  // Zera o temporizador.
         return;
     }
-    audioPlay.play();
-    intervaloId = setInterval(contagemRegressiva, 1000);
-    iniciarOuPausarBotao.textContent = "Pausar";
-    iconePlayOuPause.setAttribute('src', `/imagens/pause.png`);
+    audioPlay.play();  // Toca o som de início.
+    intervaloId = setInterval(contagemRegressiva, 1000);  // Inicia a contagem regressiva a cada segundo.
+    iniciarOuPausarBotao.textContent = "Pausar";  // Altera o texto do botão para "Pausar".
+    iconePlayOuPause.setAttribute('src', `/imagens/pause.png`);  // Altera o ícone para pausa.
 }
 
+// Função para zerar o temporizador.
 function zerar() {
-    clearInterval(intervaloId);
-    iniciarOuPausarBotao.textContent = "Começar"
-    iconePlayOuPause.setAttribute('src', `/imagens/play_arrow.png`);
-    intervaloId = null;
+    clearInterval(intervaloId);  // Limpa o intervalo de contagem regressiva.
+    iniciarOuPausarBotao.textContent = "Começar";  // Altera o texto do botão para "Começar".
+    iconePlayOuPause.setAttribute('src', `/imagens/play_arrow.png`);  // Altera o ícone para play.
+    intervaloId = null;  // Reseta o ID do intervalo.
 }
 
+// Função para formatar e mostrar o tempo na tela.
 function mostrarTempo() {
-    const tempo = tempoDecorridoEmSegundos;
-    tempoNaTela.innerHTML = `${tempo}`;
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);  // Converte segundos para um objeto de data.
+    const tempoFormatado = tempo.toLocaleString('pt-br', {minute: '2-digit', second: '2-digit'});  // Formata o tempo.
+    tempoNaTela.innerHTML = `${tempoFormatado}`;  // Exibe o tempo formatado na tela.
 }
 
-mostrarTempo()
+// Mostra o tempo inicial na tela.
+mostrarTempo();
