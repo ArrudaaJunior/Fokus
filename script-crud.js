3bbdaacdc69615c9a23de4c99fd0bdad306757a0
Arruda Junior <juniorarruda15@gmail.com>
2024-06-10 09:13:06 -0300

    Atualizando o localStorage

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,10 @@ const ulTarefas = document.querySelector('.app__section-task-list');
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
 function criarElementoTarefa(tarefa) {
     const lista = document.createElement('li');
     lista.classList.add('app__section-task-list-item');
@@ -27,6 +31,8 @@ function criarElementoTarefa(tarefa) {
     botao.onclick = () => {
         const novaDescricao = prompt("Qual é o novo nome da tarefa?");
         paragrafo.textContent = novaDescricao;
+        tarefa.descricao = novaDescricao;
+        atualizarTarefas();
     }
 
     const imagemDoBotao = document.createElement('img');
@@ -53,7 +59,7 @@ formularioAdicionarTarefa.addEventListener('submit', (evento) => {
     tarefas.push(tarefa);
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    atualizarTarefas();
     textArea.value = '';
     formularioAdicionarTarefa.classList.add('hidden');
 })
