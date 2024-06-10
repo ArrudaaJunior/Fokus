59f3bce07da30f5e0b17662026e5aea770493584
Arruda Junior <juniorarruda15@gmail.com>
2024-06-09 22:25:45 -0300

    Submetendo o formulário

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
-const botaoAdicionarTarefa = document.querySelector('app__button--add-task');
-const formularioAdicionar = document.querySelector('app__form-add-task');
+const botaoAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formularioAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+const tarefas = []
 
 botaoAdicionarTarefa.addEventListener('click', () => {
-    formularioAdicionar.classList.toggle('hidden');
+    formularioAdicionarTarefa.classList.toggle('hidden');
+})
+
+formularioAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', tarefas);
 })
