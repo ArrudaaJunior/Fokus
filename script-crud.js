5297ff6a3744bffe91a062540bb20408452ca9b5
Arruda Junior <juniorarruda15@gmail.com>
2024-06-10 07:28:59 -0300

    Escondendo o formulário ao adicionar item na lista de tarefa

--- script-crud.js
+++ script-crud.js
@@ -7,22 +7,23 @@ const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 function criarElementoTarefa(tarefa) {
     const lista = document.createElement('li');
-    lista.classList.add('.app__section-task-list-item');
+    lista.classList.add('app__section-task-list-item');
 
     const svg = document.createElement('svg');
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
 
     const paragrafo = document.createElement('p');
     paragrafo.textContent = tarefa.descricao
+    paragrafo.classList.add('app__section-task-list-item-description')
 
     const botao = document.createElement('button');
+    botao.classList.add('app_button-edit')
+
     const imagemDoBotao = document.createElement('img');
 
     imagemDoBotao.setAttribute('src', '/imagens/edit.png');
@@ -45,7 +46,11 @@ formularioAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao: textArea.value
     }
     tarefas.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    textArea.value = '';
+    formularioAdicionarTarefa.classList.add('hidden');
 })
 
 tarefas.forEach(tarefa => {
